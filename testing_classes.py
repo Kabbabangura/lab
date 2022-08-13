8f54fadedef9c9ca87952d23c94cc4f98e1a53b0
Kabbabangura <55067293+Kabbabangura@users.noreply.github.com>
2022-08-13 19:39:59 +0300

    fixed

--- testing_classes.py
+++ testing_classes.py
@@ -2,12 +2,11 @@ from classes import Television
 
 
 class Test:
-    
     def setup(self):
-        self.self.tv = Television()
+        self.tv = Television()
 
     def tear_down(self):
-        del self.self.tv
+        del self.tv
 
     def test_power(self):
         """
@@ -15,33 +14,33 @@ class Test:
         - Assert it's status has changed
         """
         assert self.tv.__str__() == "TV status: Is on = False, Channel = 0, Volume = 0"
-        self.self.tv.power()
-        assert self.self.tv.__str__() == "TV status: Is on = True, Channel = 0, Volume = 0"
+        self.tv.power()
+        assert self.tv.__str__() == "TV status: Is on = True, Channel = 0, Volume = 0"
 
     def test_channel_changing(self):
 
         """
         - Check if channel changes when TV is off
         """
-        self.self.tv.power()
-        self.self.tv.channel_up()
-        assert self.self.tv.__str__() == "TV status: Is on = False, Channel = 0, Volume = 0"
+        self.tv.power()
+        self.tv.channel_up()
+        assert self.tv.__str__() == "TV status: Is on = False, Channel = 0, Volume = 0"
 
         """
         - Check if channel up works
         """
-        self.self.tv.power()
-        self.self.tv.channel_up()
-        assert self.self.tv.__str__() == "TV status: Is on = True, Channel = 1, Volume = 0"
-        self.self.tv.channel_up()
-        self.self.tv.channel_up()
-        self.self.tv.channel_up()
-        assert self.self.tv.__str__() == "TV status: Is on = True, Channel = 0, Volume = 0"
+        self.tv.power()
+        self.tv.channel_up()
+        assert self.tv.__str__() == "TV status: Is on = True, Channel = 1, Volume = 0"
+        self.tv.channel_up()
+        self.tv.channel_up()
+        self.tv.channel_up()
+        assert self.tv.__str__() == "TV status: Is on = True, Channel = 0, Volume = 0"
 
         """
         - Check if channel down works
         """
-        self.self.tv.channel_down()
+        self.tv.channel_down()
         assert self.tv.__str__() == "TV status: Is on = True, Channel = 3, Volume = 0"
         self.tv.channel_up()
 
